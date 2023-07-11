388e902907a0dbffdbfba569e31859915895a085
marinaraSip <139236427+marinaraSip@users.noreply.github.com>
2023-07-11 22:05:51 +0530

    Update metadata_test.go

--- test/metadata_test.go
+++ test/metadata_test.go
@@ -13,6 +13,35 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+package test1
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"k8s.io/enhancements/pkg/repo"
+)
+
+func TestValidation(t *testing.T) {
+	wd, err := os.Getwd()
+	require.Nil(t, err)
+
+	rootDir := filepath.Dir(wd)
+	r, err := repo.New(rootDir)
+	require.Nil(t, err)
+
+	warnings, valErrMap, err := r.Validate()
+	require.Nil(t, err)
+	require.Len(t, valErrMap, 0)
+
+	t.Logf(
+		"KEP validation succeeded, but the following warnings occurred: %v",
+		warnings,
+	)
+}
 
 package test
 
